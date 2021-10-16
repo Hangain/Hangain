f96c89ec2f910da454db1c960ef5206019f5ff59
Hangain <92517604+Hangain@users.noreply.github.com>
2021-10-17 02:55:30 +0530

    Update meeting.py

--- api/meeting.py
+++ api/meeting.py
@@ -4,7 +4,7 @@ import json
 API_KEY = "Hangain_default_secret"
 # Hangain_URL = "http://localhost:3000/api/v1/meeting"
 # Hangain_URL = "https://Hangain.herokuapp.com/api/v1/meeting"
-Hangain_URL = "https://Hangain.up.railway.app/api/v1/meeting"
+Hangain_URL = "https://hangain.herokuapp.com/api/v1/meeting"
 
 headers = {
     "authorization": API_KEY,
