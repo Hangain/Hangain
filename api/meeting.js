c580869f122fd12fc41e2691174cfc05da0cb60a
Hangain <92517604+Hangain@users.noreply.github.com>
2021-10-17 02:56:03 +0530

    Update meeting.js

--- api/meeting.js
+++ api/meeting.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const API_KEY = 'Hangain_default_secret';
 // const Hangain_URL = "http://localhost:3000/api/v1/meeting";
 // const Hangain_URL = "https://Hangain.herokuapp.com/api/v1/meeting";
-const Hangain_URL = 'https://Hangain.up.railway.app/api/v1/meeting';
+const Hangain_URL = 'https://hangain.herokuapp.com/api/v1/meeting';
 
 function getResponse() {
     return fetch(Hangain_URL, {
