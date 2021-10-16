ef6640d5d10f59419b086137558f3ca0ab1f778c
Hangain <92517604+Hangain@users.noreply.github.com>
2021-10-17 02:58:00 +0530

    Update meeting.sh

--- api/meeting.sh
+++ api/meeting.sh
@@ -3,7 +3,7 @@
 API_KEY="Hangain_default_secret"
 # Hangain_URL="http://localhost:3000/api/v1/meeting"
 # Hangain_URL=https://Hangain.herokuapp.com/api/v1/meeting
-Hangain_URL=https://Hangain.up.railway.app/api/v1/meeting
+Hangain_URL=https://hangain.herokuapp.com/api/v1/meeting
 
 curl $Hangain_URL \
     --header "authorization: $API_KEY" \
